4844b3704c6212e55b42bd24c5c9974c49ece9cf
stettix <jan.stette@gmail.com>
2015-10-24 14:24:47 +0100

    Rest of chapter 5 exercises.

--- chapter_5.ml
+++ chapter_5.ml
@@ -14,6 +14,29 @@ let rec msort l =
     [] -> []
   | [x] -> [x]
   | _ ->
-      let left = take (length l / 2) l in
-        let right = drop (length l / 2) l in
-          merge (msort left) (msort right)
+      let half = length l / 2 in
+        let left = take half l in
+          let right = drop half l in
+            merge (msort left) (msort right)
+
+let rec insert x l cmp =
+  match l with
+    [] -> [x]
+  | h :: t ->
+    if cmp x h then x :: l
+      else h :: (insert x t cmp)
+
+let rec sort l =
+  match l with
+    [] -> []
+  | h :: t -> insert h (sort t) (<=)
+
+let rec rev_sort l =
+  match l with
+    [] -> []
+  | h :: t -> insert h (rev_sort t) (>=)
+
+let rec is_sorted l =
+  match l with
+    x :: y :: t -> (x <= y) && (is_sorted (y :: t))
+  | _ -> true
--- chapter_5_test.ml
+++ chapter_5_test.ml
@@ -10,7 +10,28 @@ let test_msort test_ctxt =
 	assert_equal [1; 3; 5; 11; 12] (msort l1);
 	assert_equal [2; 4; 6] (msort l2);;
 
+let test_sort test_ctxt =
+	assert_equal [] (sort []);
+	assert_equal [1; 3; 5; 11; 12] (sort l1);
+	assert_equal [2; 4; 6] (sort l2);;
+
+let test_rev_sort test_ctxt =
+	assert_equal [] (rev_sort []);
+	assert_equal [12; 11; 5; 3; 1] (rev_sort l1);
+	assert_equal [6; 4; 2] (rev_sort l2);;
+
+let test_is_sorted test_ctxt =
+	assert_equal true (is_sorted []);
+	assert_equal true (is_sorted [1]);
+	assert_equal false (is_sorted l1);
+	assert_equal false (is_sorted l2);
+	assert_equal true (is_sorted (sort l1));
+	assert_equal true (is_sorted (sort l2));;
+
 let suite =
   "Chapter 5 test suite">:::
-    ["test msort">:: test_msort]
+    ["test msort">:: test_msort;
+    "test_sort">:: test_sort;
+    "test_rev_sort">:: test_rev_sort;
+    "test_is_sorted">:: test_is_sorted]
   ;;
