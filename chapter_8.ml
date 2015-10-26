7d728c72cc399265525d217b7d5cfdbb35a0814f
stettix <jan.stette@gmail.com>
2015-10-26 22:19:09 +0000

    More fun with dictionaries.

--- chapter_8.ml
+++ chapter_8.ml
@@ -1,3 +1,5 @@
+open Chapter_4;;
+
 let rec numkeys d =
   match d with
     [] -> 0
@@ -15,3 +17,19 @@ let rec dict_of_lists l1 l2 =
     [], [] -> []
   | h1 :: t1, h2 :: t2 -> (h1, h2) :: dict_of_lists t1 t2
   | _ -> raise (Invalid_argument "Lists not of equal size")
+
+let rec split d =
+  match d with
+    [] -> [], []
+  | (k, v) :: t ->
+    let tk, tv = split t in
+      k :: tk, v :: tv 
+
+let rec dict_of_pairs l = 
+  let rec loop l seenKeys =
+    match l, seenKeys with
+	  [], _ -> []
+	| ((k, v) :: t), _ ->
+	  if member k seenKeys then loop t seenKeys
+	    else (k, v) :: (loop t (k :: seenKeys))
+  in loop l []
--- chapter_8_test.ml
+++ chapter_8_test.ml
@@ -20,8 +20,22 @@ let test_dict_of_lists test_ctxt =
   assert_equal [1, 10; 2, 20; 3, 30] (dict_of_lists [1; 2; 3] [10; 20; 30]);
   assert_raises (Invalid_argument "Lists not of equal size") (fun () -> dict_of_lists [] [1]);;
 
+let test_split test_ctxt =
+  assert_equal ([], []) (split []);
+  assert_equal ([1], [10]) (split [1, 10]);
+  assert_equal ([1; 2; 3], [10; 20; 30]) (split [1, 10; 2, 20; 3, 30]);;
+
+let test_dict_of_pairs test_ctxt =
+  assert_equal [] (dict_of_pairs []);
+  assert_equal [1, 10] (dict_of_pairs [1, 10]);
+  assert_equal [1, 10; 2, 20] (dict_of_pairs [1, 10; 2, 20]);
+  assert_equal [1, 11] (dict_of_pairs [1, 11; 1, 10]);
+  assert_equal [1, 10; 2, 20] (dict_of_pairs [1, 10; 2, 20; 1, 11]);;
+
 let suite =
   "Chapter 8 test suite">:::
     ["test numkeys">:: test_numkeys;
      "test replace">:: test_replace;
-     "test dict_of_lists">:: test_dict_of_lists]
+     "test dict_of_lists">:: test_dict_of_lists;
+     "test test_split">:: test_split;
+     "test test_dict_of_pairs">:: test_dict_of_pairs]
